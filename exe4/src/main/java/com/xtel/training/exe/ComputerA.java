81b90095699c23dbdc7d82771e27dd712d5cb415
Phunguyen <yolosolo25@example.com>
2020-11-16 12:05:21 +0700

    update_chat

--- exe4/src/main/java/com/xtel/training/exe/ComputerA.java
+++ exe4/src/main/java/com/xtel/training/exe/ComputerA.java
@@ -13,11 +13,11 @@ public class ComputerA {
         Socket socket = new Socket("localhost", 3333);
         DataInputStream din = new DataInputStream(socket.getInputStream());
         DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str = "", str2 = "";
         while (!str.equals("stop")) {
-//            str = br.readLine();
+            str = br.readLine();
             dout.writeUTF(randString.randomAlphaNumeric(numberOfCharactor));
             dout.flush();
             str2 = din.readUTF();
